2acf58f1dde1bd7ac11c9423834c314407ed4c96
uplifehig <brunochimenes_@hotmail.com>
2023-01-01 19:16:49 -0300

    att

--- js/modules/imagem-trocando.js
+++ js/modules/imagem-trocando.js
@@ -1,19 +1,21 @@
 export default function initImagemTrocando() {
-  const main = document.querySelector("main");
-  setInterval(trocarImagem, 3000);
-  setInterval(trocarImagemDois, 5000);
-  function trocarImagem() {
-    main.style.backgroundImage = "url('/imagens/introducao/introducao2.png')";
-    main.style.backgroundRepeat = "no-repeat";
-    main.style.backgroundSize = "cover";
-    main.style.backgroundPosition = "center";
-    main.style.padding = "100px 0px";
-  }
-  function trocarImagemDois() {
-    main.style.backgroundImage = "url('/imagens/introducao/introducao.png')";
-    main.style.backgroundRepeat = "no-repeat";
-    main.style.backgroundSize = "cover";
-    main.style.backgroundPosition = "center";
-    main.style.padding = "100px 0px";
-  }
+  window.onload = function () {
+    const main = document.querySelector("main");
+    setInterval(trocarImagem, 3000);
+    setInterval(trocarImagemDois, 5000);
+    function trocarImagem() {
+      main.style.backgroundImage = "url('/imagens/introducao/introducao2.png')";
+      main.style.backgroundRepeat = "no-repeat";
+      main.style.backgroundSize = "cover";
+      main.style.backgroundPosition = "center";
+      main.style.padding = "100px 0px";
+    }
+    function trocarImagemDois() {
+      main.style.backgroundImage = "url('/imagens/introducao/introducao.png')";
+      main.style.backgroundRepeat = "no-repeat";
+      main.style.backgroundSize = "cover";
+      main.style.backgroundPosition = "center";
+      main.style.padding = "100px 0px";
+    }
+  };
 }
