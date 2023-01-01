a64a4f0fe0f96f6a867c4282354ca038450a8f64
uplifehig <brunochimenes_@hotmail.com>
2023-01-01 19:47:59 -0300

    att

--- js/modules/imagem-trocando.js
+++ js/modules/imagem-trocando.js
@@ -4,16 +4,14 @@ export default function initImagemTrocando() {
     setInterval(trocarImagem, 3000);
     setInterval(trocarImagemDois, 5000);
     function trocarImagem() {
-      main.style.backgroundImage =
-        "url('../imagens/introducao/introducao2.png')";
+      main.style.backgroundImage = "url('/imagens/introducao/introducao2.png')";
       main.style.backgroundRepeat = "no-repeat";
       main.style.backgroundSize = "cover";
       main.style.backgroundPosition = "center";
       main.style.padding = "100px 0px";
     }
     function trocarImagemDois() {
-      main.style.backgroundImage =
-        "url('../imagens/introducao/introducao.png')";
+      main.style.backgroundImage = "url('/imagens/introducao/introducao.png')";
       main.style.backgroundRepeat = "no-repeat";
       main.style.backgroundSize = "cover";
       main.style.backgroundPosition = "center";
